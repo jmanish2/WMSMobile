528d64fa8c62502f2016bd50a690ca83196a68ce
Shivani shrimali <shivani.shrimali@appsphere.in>
2024-08-21 16:49:42 +0530

    Version updated

--- src/screens/authenticated/Receiving/ReceivingScreen.tsx
+++ src/screens/authenticated/Receiving/ReceivingScreen.tsx
@@ -1,14 +1,9 @@
 import React, { useState } from 'react';
-import { View, Button, Image, StyleSheet } from 'react-native';
-import {
-  launchCamera,
-  launchImageLibrary,
-  Asset,
-  ImageLibraryOptions,
-  CameraOptions,
-} from 'react-native-image-picker';
+import { View, Button, Image, StyleSheet, Alert } from 'react-native';
+import { launchCamera, launchImageLibrary, Asset, CameraOptions, ImagePickerResponse } from 'react-native-image-picker';
 import { request, PERMISSIONS } from 'react-native-permissions';
 import MLKitBarcodeScanner, { Barcode } from '@react-native-ml-kit/barcode-scanning';
+import ImageEditor from '@react-native-community/image-editor';
 import { Text } from 'react-native-paper';
 
 const ImagePickerExample: React.FC = () => {
@@ -27,40 +22,55 @@ const ImagePickerExample: React.FC = () => {
     }
   };
 
-  // Handle image picker
-  // const openImagePicker = () => {
-  //   const options: ImageLibraryOptions = {
-  //     mediaType: 'photo',
-  //     quality: 1,
-  //   };
+  // Crop and scan image
+  const cropAndScanImage = async (uri: string, width: number, height: number) => {
+    try {
+      // Define crop sections with offset and size
+      const cropSections = [
+        { offset: { x: 0, y: 0 }, size: { width: width / 2, height: height / 2 } },
+        { offset: { x: width / 2, y: 0 }, size: { width: width / 2, height: height / 2 } },
+        { offset: { x: 0, y: height / 2 }, size: { width: width / 2, height: height / 2 } },
+        { offset: { x: width / 2, y: height / 2 }, size: { width: width / 2, height: height / 2 } },
+      ];
 
-  //   launchImageLibrary(options, async response => {
-  //     console.log('Image picker response:', response);
-  //     if (response.didCancel) {
-  //       console.log('User cancelled image picker');
-  //     } else if (response.errorCode) {
-  //       console.log('ImagePicker Error: ', response.errorCode);
-  //     } else if (response.assets && response.assets.length > 0) {
-  //       const selectedAsset: Asset = response.assets[0];
-  //       const uri = selectedAsset.uri;
-  //       console.log('Selected URI:', uri);
-  //       if (uri) {
-  //         setImageUri(uri);
-  //         try {
-  //           const barcodeResults: Barcode[] = await MLKitBarcodeScanner.scan(uri);
-  //           console.log('Barcode results:', barcodeResults);
+      const barcodeResults: string[] = [];
 
-  //           // Extract values from barcode results
-  //           const newBarcodes = barcodeResults.map(barcode => barcode.value || '');
+      for (const section of cropSections) {
+        const { offset, size } = section;
 
-  //           setBarcodes(prevBarcodes => [...new Set([...prevBarcodes, ...newBarcodes])]);
-  //         } catch (error) {
-  //           console.error('Barcode scanning error:', error);
-  //         }
-  //       }
-  //     }
-  //   });
-  // };
+        const cropData = {
+          offset: {
+            x: offset.x,
+            y: offset.y,
+          },
+          size: {
+            width: size.width,
+            height: size.height,
+          },
+        };
+
+        // Perform cropping
+        const cropResult = await ImageEditor.cropImage(uri, cropData);
+
+        // Ensure cropResult has a uri property
+        if (cropResult?.uri) {
+          const croppedImageUri = cropResult.uri;
+
+          // Scan the cropped image
+          const results: Barcode[] = await MLKitBarcodeScanner.scan(croppedImageUri);
+          results.forEach(barcode => {
+            if (barcode.value) {
+              barcodeResults.push(barcode.value);
+            }
+          });
+        }
+      }
+
+      setBarcodes([...new Set(barcodeResults)]);
+    } catch (error) {
+      console.error('Image cropping and scanning error:', error);
+    }
+  };
 
   // Handle camera
   const openCamera = async () => {
@@ -75,36 +85,54 @@ const ImagePickerExample: React.FC = () => {
       quality: 1,
     };
 
-    launchCamera(options, async response => {
+    launchCamera(options, async (response: ImagePickerResponse) => {
       console.log('Camera response:', response);
-      if (response.didCancel) {
-        console.log('User cancelled camera');
-      } else if (response.errorCode) {
-        console.log('Camera Error: ', response.errorCode);
-      } else if (response.assets && response.assets.length > 0) {
-        const capturedAsset: Asset = response.assets[0];
-        const uri = capturedAsset.uri;
-        if (uri) {
-          setImageUri(uri);
-          try {
-            const barcodeResults: Barcode[] = await MLKitBarcodeScanner.scan(uri);
-            console.log('Barcode results:', barcodeResults);
-
-            // Extract values from barcode results
-            const newBarcodes = barcodeResults.map(barcode => barcode.value || '');
-
-            setBarcodes(prevBarcodes => [...new Set([...prevBarcodes, ...newBarcodes])]);
-          } catch (error) {
-            console.error('Barcode scanning error:', error);
-          }
-        }
-      }
+      handleImageResponse(response);
     });
+  };
+
+  // Handle image library
+  const openImageLibrary = async () => {
+    const options: CameraOptions = {
+      mediaType: 'photo',
+    };
+
+    launchImageLibrary(options, async (response: ImagePickerResponse) => {
+      console.log('Image Library response:', response);
+      handleImageResponse(response);
+    });
+  };
+
+  // Handle image picker response
+  const handleImageResponse = async (response: ImagePickerResponse) => {
+    if (response.didCancel) {
+      console.log('User cancelled image picker');
+    } else if (response.errorCode) {
+      console.log('Image Picker Error: ', response.errorCode);
+      Alert.alert('Image Picker Error', response.errorCode);
+    } else if (response.assets && response.assets.length > 0) {
+      const capturedAsset: Asset = response.assets[0];
+      const uri = capturedAsset.uri;
+      const width = capturedAsset.width;
+      const height = capturedAsset.height;
+      if (uri && width && height) {
+        setImageUri(uri);
+        await cropAndScanImage(uri, width, height);
+      }
+    }
+  };
+
+  // Reset function
+  const reset = () => {
+    setImageUri(undefined);
+    setBarcodes([]);
   };
 
   return (
     <View style={styles.container}>
       <Button title="Take a Photo" onPress={openCamera} />
+      <Button title="Select from Gallery" onPress={openImageLibrary} />
+      <Button title="Reset" onPress={reset} />
       {imageUri && <Image source={{ uri: imageUri }} style={styles.image} />}
       <Text style={styles.barcodeText}>
         Scanned Barcodes: {barcodes.join(', ')}
